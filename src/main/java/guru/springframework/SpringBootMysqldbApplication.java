44ca07c4d80dcd57177609e473fc97944acabe67
johan974 <jj974@upcmail.nl>
2017-09-13 19:23:33 +0200

    New: ready for openshift.

--- src/main/java/guru/springframework/SpringBootMysqldbApplication.java
+++ src/main/java/guru/springframework/SpringBootMysqldbApplication.java
@@ -2,14 +2,28 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 // URL: http://localhost:8080
 // URL: http://localhost:8080/product/list
 
+/* JAR version
 @SpringBootApplication
 public class SpringBootMysqldbApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMysqldbApplication.class, args);
+	}
+} */
+
+// WAR version
+@SpringBootApplication
+public class SpringBootMysqldbApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootMysqldbApplication .class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqldbApplication .class, args);
 	}
 }
